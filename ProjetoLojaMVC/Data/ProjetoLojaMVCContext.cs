496cd08309db043598f89ebaaddfe1ead09ee402
Denis <denisfioravante722@hotmail.com>
2019-04-19 19:44:52 -0300

    CRUD Scafoolding

---
+++ ProjetoLojaMVC/Data/ProjetoLojaMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoLojaMVC.Models
+{
+    public class ProjetoLojaMVCContext : DbContext
+    {
+        public ProjetoLojaMVCContext (DbContextOptions<ProjetoLojaMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoLojaMVC.Models.Departament> Departament { get; set; }
+    }
+}
